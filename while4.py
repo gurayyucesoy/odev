393e1bf7e31b583e16670032e851323ce0e149a8
Güray Yücesoy <37017987+gurayyucesoy@users.noreply.github.com>
2018-03-22 14:41:47 +0300

    Add files via upload

---
+++ while4.py
@@ -0,0 +1,13 @@
+calisan=50
+yevmiye=90
+aylik_mesai=0
+haftalik_maas=630
+aylik_maas=0
+while aylik_mesai<=22:
+    haftalik_mesai=int(input("Haftalık mesai:"))
+    aylik_mesai=haftalik_mesai*4
+    haftalik_maas=haftalik_maas+(haftalik_mesai*yevmiye*0.10)
+    aylik_maas=aylik_maas+haftalik_maas*4
+    print("Aylik maas:",aylik_maas)
+else:
+    print("Aylik mesai 22 saatten fazla olamaz") 
